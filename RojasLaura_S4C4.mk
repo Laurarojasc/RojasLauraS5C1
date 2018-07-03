2c7f6603c46ae234b05d0eaee169b4e69308c353
Laura Rojas <ca.triana957@uniandes.edu.co>
2018-07-03 12:03:38 -0500

    RojasLaura_S4C4.mk

---
+++ RojasLaura_S4C4.mk
@@ -0,0 +1,28 @@
+
+
+
+#el latex no depende de nada 
+
+resultados.pdf : plot.pdf plot1.pdf resultados.tex 
+	
+	pdflatex resultados.tex
+
+plot.pdf: plotdatos.py datos.dat
+	python plotdatos.py
+
+plot1.pdf: plotdatos1.py datos1.dat
+	python plotdatos1.py
+
+	
+datos.dat : makedatos.py
+	python makedatos.py>datos.dat
+
+
+datos1.dat : makedatos1.py
+	python makedatos1.py>datos1.dat
+ 
+
+#guardo makedatos1 en datoa1.dat 
+
+	
+	  
